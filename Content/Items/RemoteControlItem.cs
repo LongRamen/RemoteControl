9791a7ba4910cbe45e24b10447a32b0386f1528b
LongRamen <106673028+LongRamen@users.noreply.github.com>
2025-11-24 18:49:40 +0000

    v1.2
    - Added an icon_small.png
    - Cleaned up code (most notably, call HitWireSingle directly via reflection instead of using a copy)

--- Content/Items/RemoteControlItem.cs
+++ Content/Items/RemoteControlItem.cs
@@ -1,6 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
+using System.Reflection;
 using Terraria;
 using Terraria.Audio;
 using Terraria.ID;
@@ -10,104 +11,105 @@ namespace RemoteControl.Content.Items;
 
 public class RemoteControlItem : ModItem
 {
-	private static short glowMaskID = GlowMaskID.None;
-	private static Asset<Texture2D> glowmaskTextureOn;
-	private static Asset<Texture2D> glowmaskTextureOff;
-	private static bool On = false;
-	private static SoundStyle BeepSoundStyle = new SoundStyle("RemoteControl/Assets/Sounds/RemoteControlBeep") with { PitchVariance = 0.015625f };
+    private static short glowMaskID = GlowMaskID.None;
+    private static Asset<Texture2D> glowmaskTextureOn;
+    private static Asset<Texture2D> glowmaskTextureOff;
+    private static bool On = false;
+    private static SoundStyle BeepSoundStyle = new SoundStyle("RemoteControl/Assets/Sounds/RemoteControlBeep") with { PitchVariance = 0.015625f };
 
-	public override void Load()
-	{
-		glowmaskTextureOn  = ModContent.Request<Texture2D>(Texture + "_GlowmaskOn",  AssetRequestMode.ImmediateLoad);
-		glowmaskTextureOff = ModContent.Request<Texture2D>(Texture + "_GlowmaskOff", AssetRequestMode.ImmediateLoad);
+    private static MethodInfo HitWireSingle;
 
-		if (Main.netMode != NetmodeID.Server && !ModLoader.HasMod("TerrariaOverhaul"))
-		{
-			glowMaskID = Common.GlowmaskHelper.AddGlowMask(glowmaskTextureOff);
-		}
-	}
+    public override void Load()
+    {
+        glowmaskTextureOn  = ModContent.Request<Texture2D>(Texture + "_GlowmaskOn",  AssetRequestMode.ImmediateLoad);
+        glowmaskTextureOff = ModContent.Request<Texture2D>(Texture + "_GlowmaskOff", AssetRequestMode.ImmediateLoad);
 
-	public override void SetStaticDefaults()
-	{
-		Item.ResearchUnlockCount = 1;
+        if (Main.netMode != NetmodeID.Server && !ModLoader.HasMod("TerrariaOverhaul"))
+        {
+            glowMaskID = Common.GlowmaskHelper.AddGlowMask(glowmaskTextureOff);
+        }
 
-		ItemID.Sets.DuplicationMenuToolsFilter[Type] = true;
-		ItemID.Sets.GamepadWholeScreenUseRange[Type] = true;
-	}
+        HitWireSingle = typeof(Wiring).GetMethod("HitWireSingle", BindingFlags.NonPublic | BindingFlags.Static);
+    }
 
-	public override void SetDefaults()
-	{
-		Item.maxStack = 1;
-		Item.width = 30;
-		Item.height = 30;
-		Item.value = 25000;
-		Item.rare = ItemRarityID.Orange;
-		Item.mech = true;
+    public override void SetStaticDefaults()
+    {
+        Item.ResearchUnlockCount = 1;
 
-		if (!ModLoader.HasMod("TerrariaOverhaul"))
-		{
-			Item.glowMask = glowMaskID;
+        ItemID.Sets.DuplicationMenuToolsFilter[Type] = true;
+        ItemID.Sets.GamepadWholeScreenUseRange[Type] = true;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.maxStack = 1;
+        Item.width = 30;
+        Item.height = 30;
+        Item.value = 25000;
+        Item.rare = ItemRarityID.Orange;
+        Item.mech = true;
+
+        if (!ModLoader.HasMod("TerrariaOverhaul"))
+        {
+            Item.glowMask = glowMaskID;
             Item.holdStyle = ItemHoldStyleID.HoldHeavy;
         }
 
         Item.useTime = 10;
         Item.useAnimation = Item.useTime;
-		Item.useStyle = ItemUseStyleID.Shoot;
-		Item.UseSound = BeepSoundStyle;
-	}
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.UseSound = BeepSoundStyle;
+    }
 
-	public override bool AltFunctionUse(Player player) => true;
+    public override bool AltFunctionUse(Player player) => true;
 
-	public override bool? UseItem(Player player)
-	{
-		if (player.JustDroppedAnItem) return true;
+    public override bool? UseItem(Player player)
+    {
+        if (player.whoAmI != Main.myPlayer || player.JustDroppedAnItem) return true;
 
-		if (Main.netMode != NetmodeID.Server && !ModLoader.HasMod("TerrariaOverhaul"))
-		{
-			On = On || Common.GlowmaskHelper.EditGlowMask(glowMaskID, glowmaskTextureOn);
-		}
+        if (Main.netMode != NetmodeID.Server && !ModLoader.HasMod("TerrariaOverhaul"))
+        {
+            On |= Common.GlowmaskHelper.EditGlowMask(glowMaskID, glowmaskTextureOn);
+        }
 
-		if (player.whoAmI != Main.myPlayer) return true;
+        Point pos = Main.MouseWorld.ToTileCoordinates();
+        if (!WorldGen.InWorld(pos.X, pos.Y)) return true;
 
-		try
-		{
-			Point pos = Main.MouseWorld.ToTileCoordinates();
-			if (!WorldGen.InWorld(pos.X, pos.Y)) return true;
+        if (player.altFunctionUse == 2)
+        {
+            HitWireSingle.Invoke(null, [pos.X, pos.Y]); // Common.WiringHelper.HitWireSingle(pos.X, pos.Y);
+        }
+        else
+        {
+            Wiring.TripWire(pos.X, pos.Y, 1, 1);
+        }
 
-			if (player.altFunctionUse == 2) Common.WiringHelper.HitWireSingle(pos.X, pos.Y);
-			else Wiring.TripWire(pos.X, pos.Y, 1, 1);
+        return true;
+    }
+    
+    public override void UseStyle(Player player, Rectangle heldItemFrame)
+    {
+        if (ModLoader.HasMod("TerrariaOverhaul")) return;
 
-			return true;
-		}
-		catch
-		{
-			return true;
-		}
-	}
-	
-	public override void UseStyle(Player player, Rectangle heldItemFrame)
-	{
-		if (ModLoader.HasMod("TerrariaOverhaul")) return;
-
-		if (player.itemTime == 0)
-		{
-			player.ApplyItemTime(Item);
-		}
-		else if (player.itemTime == 1)
-		{
-			if (On && Main.netMode != NetmodeID.Server)
-			{
-				Common.GlowmaskHelper.EditGlowMask(glowMaskID, glowmaskTextureOff);
-				On = false;
-			}
-		}
-	}
+        if (player.itemTime == 0)
+        {
+            player.ApplyItemTime(Item);
+        }
+        else if (player.itemTime == 1)
+        {
+            if (On && Main.netMode != NetmodeID.Server)
+            {
+                Common.GlowmaskHelper.EditGlowMask(glowMaskID, glowmaskTextureOff);
+                On = false;
+            }
+        }
+    }
 }
 
 public class RemoteControlShop : GlobalNPC
 {
-	public override void ModifyShop(NPCShop shop)
-	{
-		if (shop.NpcType == NPCID.Mechanic) shop.Add(ModContent.ItemType<RemoteControlItem>());
-	}
+    public override void ModifyShop(NPCShop shop)
+    {
+        if (shop.NpcType == NPCID.Mechanic) shop.Add(ModContent.ItemType<RemoteControlItem>());
+    }
 }
